9d84576aa6c04fc0346f5d99663ccdc9069e3f3a
claudio <cdjohnnatha@gmail.com>
2020-05-12 23:16:33 -0400

    update timesheet-schedule-hours to automatically init day based in current day

--- api/db/models/timesheet-schedule-hours.js
+++ api/db/models/timesheet-schedule-hours.js
@@ -1,6 +1,7 @@
 const TimesheetScheduleHoursDataTypes = require('./data-types/timesheets/timesheet-schedule-hours-data-types');
 const ModelSettings = require('../model-settings');
 const { WEEK_DAYS_ENUM } = require('../../utils/const-utils');
+const WeekDayHelpers = require('../../helpers/week-day-helpers')
 
 module.exports = (db) => {
   const TimesheetScheduleHour = db.define('TimesheetScheduleHours', TimesheetScheduleHoursDataTypes, {
@@ -10,15 +11,22 @@ module.exports = (db) => {
 
   TimesheetScheduleHour.buildByAvailableMinutesArray = (available_minutes_per_day) => {
     let highest_freetime_minutes_of_week = 0;
-    const timesheet_schedule_hours = available_minutes_per_day.map((time, dayOfWeek) => {
+    const weekDayHelper = new WeekDayHelpers();
+    let available_minute_today = null;
+    const timesheet_schedule_hours = available_minutes_per_day.map((time, index) => {
+      
       if (time > highest_freetime_minutes_of_week) {
         highest_freetime_minutes_of_week = time;
       };
-
-      return {
-        day_of_week: WEEK_DAYS_ENUM[dayOfWeek],
+      console.log('[weekDayHelper.weekDay]', weekDayHelper.weekDay);
+      available_minute_today = {
+        day_of_week: `${weekDayHelper.weekDay}`,
         available_minutes: time,
       };
+
+      weekDayHelper.nextDay();
+
+      return available_minute_today;
     });
     return {
       highest_freetime_minutes_of_week,
