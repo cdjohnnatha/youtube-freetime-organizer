e908b4ba3d3d444dc4a73ef33769c17663df87b0
claudio <cdjohnnatha@gmail.com>
2020-05-13 01:03:48 -0400

    added to circleci the token secret used for generate jwt

--- api/db/seeders/20200510035646-create_default_users.js
+++ api/db/seeders/20200510035646-create_default_users.js
@@ -3,12 +3,19 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
   up: async (queryInterface) => {
+    await queryInterface.bulkInsert('auth', [{
+      email: 'user@test.com.br',
+      password: '123456789',  
+    }]);
+
+    const [auth] = await queryInterface.sequelize.query("SELECT id FROM auth WHERE email like 'user@test.com.br'");
     return queryInterface.bulkInsert(
       'users',
       [
         {
           first_name: faker.name.firstName(),
           last_name: faker.name.lastName(),
+          auth_id: auth[0].id,
           created_at: new Date(),
           updated_at: new Date(),
         },
