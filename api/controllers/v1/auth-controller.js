b74283bc48b4630c954083cf64dde337e828d95c
claudio <cdjohnnatha@gmail.com>
2020-05-12 23:17:05 -0400

    updated signIn error message to get from i18n files

--- api/controllers/v1/auth-controller.js
+++ api/controllers/v1/auth-controller.js
@@ -1,10 +1,11 @@
 const { houstonClientErrors } = require('houston-errors');
+const i18n = require('i18n');
 const { signupSchema, authEmailSchema } = require('./schemas/auth-schemas');
 const { createUserRepository } = require('../../repositories/users-respository');
 const logger = require('../../config/logger');
 const { generateJWT } = require('../../helpers/token-helpers');
 const { Auth, Users } = require('../../db/models');
-
+const { formatErrorMessage } = require('../../helpers/formatter-helpers')
 const { BAD_REQUEST } = houstonClientErrors;
 
 const signUpController = async ({ body }, response) => {
@@ -41,12 +42,12 @@ const authEmailProviderController = async ({ body, ...props }, response) => {
         const jwt = await generateJWT({ user_id: auth.dataValues.users.id });
         response.status(200).send(jwt);
       } else {
-        response.status(BAD_REQUEST.code).send('Invalid email or password');
+        response.status(BAD_REQUEST.code).send(i18n.__('error.invalid_sign_in'));
       }
     }
   } catch (error) {
     logger.systemLogLevel({ error, level: 'error' });
-    response.status(BAD_REQUEST.code).send(error);
+    response.status(BAD_REQUEST.code).send(formatErrorMessage(error));
   }
 };
 
